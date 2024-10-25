82605cb7a775821fde6ee1585ed81a249bea3c90
Hasnat79 <hasnatabdullah79@gmail.com>
2024-10-25 09:25:23 -0500

    timechat ckpt setup

---
+++ setups/download_timechat_ckpts.sh
@@ -0,0 +1,4 @@
+wget https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/eva_vit_g.pth
+wget https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/InstructBLIP/instruct_blip_vicuna7b_trimmed.pth
+git clone https://huggingface.co/DAMO-NLP-SG/Video-LLaMA-2-7B-Finetuned
+git clone https://huggingface.co/ShuhuaiRen/TimeChat-7b
