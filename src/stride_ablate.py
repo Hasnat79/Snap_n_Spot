cd097043b5c87abbfe946298c79c817ab2b5cced
hasnat79 <hasnatabdullah79@gmail.com>
2024-12-10 21:55:28 -0600

    ablate study

---
+++ src/stride_ablate.py
@@ -0,0 +1,75 @@
+from data_configs import DATASETS
+import argparse
+import numpy as np
+import json
+from tqdm import tqdm
+from vlm_localizer import localize
+import os
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Evaluation for training-free video temporal grounding.')
+    parser.add_argument('--dataset', default='uag_oops', type=str, help='Specify the dataset. See supported datasets in data_configs.py.')
+    parser.add_argument('--split', default='default', type=str, help='Specify the split. See supported splits in data_configs.py.')
+    return parser.parse_args()
+
+
+def calc_iou(candidates, gt):
+    start, end = candidates[:,0], candidates[:,1]
+    s, e = gt[0], gt[1]
+    inter = np.minimum(end, e) - np.maximum(start, s)
+    union = np.maximum(end, e) - np.minimum(start, s)
+    return inter.clip(min=0) / union
+
+
+def eval(data, feature_path, stride, max_stride_factor, pad_sec=0.0):
+    ious = []
+    thresh = np.array([0.3, 0.5, 0.7])
+    recall = np.array([0, 0, 0])
+    
+    pbar = tqdm(data.items())
+    for vid, ann in pbar:
+        query_json = []
+        for i in range(len(ann['sentences'])):
+            query_json.append({'descriptions': [ann['sentences'][i]]})
+
+        duration = ann['duration'] if 'duration' in ann else ann['video_duration']
+        video_feature_path = os.path.join(feature_path, vid+'.npy')
+        video_feature = np.load(video_feature_path)
+        if pad_sec > 0:
+            pad_noise = np.random.randn(round(video_feature.shape[0] / duration * pad_sec), video_feature.shape[1], video_feature.shape[2])
+            video_feature = np.concatenate([pad_noise, video_feature], axis=0)
+            duration += pad_sec
+
+        ans = localize(video_feature, duration, query_json, stride, int(video_feature.shape[0] * max_stride_factor))
+        for i in range(len(ans)):
+            s, e = ann['timestamps'][i]
+            s, e = s + pad_sec, e + pad_sec
+
+            sp, ep = ans[i]['response'][0]['start'], ans[i]['response'][0]['end']
+            iou_ = (min(e, ep) - max(s, sp)) / (max(e, ep) - min(s, sp))
+            ious.append(max(iou_, 0))
+            recall += thresh <= iou_
+        pbar.set_postfix({"mIoU": sum(ious) / len(ious), 'recall': str(recall / len(ious))})
+
+    print('mIoU:', sum(ious) / len(ious))
+    for th, r in zip(thresh, recall):
+        print(f'R@{th}:', r / len(ious))
+
+
+if __name__=='__main__':
+    args = get_args()
+    assert args.dataset in DATASETS, 'Unsupported dataset. To evaluate other datasets, please add the configuration in data_configs.py.'
+    dataset = DATASETS[args.dataset]
+    assert args.split in dataset['splits'], 'Unsupported split. To evaluate other split, please add the configuration in data_configs.py.'
+    
+    print('Evaluating', args.dataset, args.split)
+
+
+    with open(dataset['splits'][args.split]['annotation_file']) as f:
+        data = json.load(f)
+
+    for stride in [32, 64, 128]:
+        print('Stride:', stride)
+        eval(data, dataset['feature_path'], stride, dataset['max_stride_factor'], dataset['splits'][args.split]['pad_sec'])
+        
